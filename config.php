4f302f277b8d0c1325edda5b826ed5085610579f
JoMds <Giannis58@Hotmail.com>
2022-11-05 23:12:19 +0200

    ..

--- config.php
+++ config.php
@@ -4,12 +4,10 @@ define('DB_USERNAME','root');
 define('DB_PASSWORD','1234');
 define('DB_NAME','blogTest');
 
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-
-if($link == false) {
-    die("ERROR: Could not connect" . mysqli_connect_error());
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
 
 ?>
